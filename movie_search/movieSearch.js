dc075fb8d1a816e2011495c5f14b8bb274e8500c
sebaandino <126474310+sebaandino@users.noreply.github.com>
2024-04-30 21:30:43 -0300

    se agrego la funcionalidad del buscador

--- movie_search/movieSearch.js
+++ movie_search/movieSearch.js
@@ -1,7 +1,15 @@
+
+// se obtiene la parte de la URL que contiene los parámetros
+const urlParams = new URLSearchParams(window.location.search);
+
+// se extrae el valor del parametro "query"
+const query = urlParams.get('query');
+
+// prueba del parametro
+console.log('peli a buscar:', query);
+
 const apiKey = "8abb33cb7535fb63ffe506dd0dfe0021";
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YWJiMzNjYjc1MzVmYjYzZmZlNTA2ZGQwZGZlMDAyMSIsInN1YiI6IjY2MmZlMWMxMWYwMjc1MDEyNzE0ZWFhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0UWZOlQ4GkikRMacrfn28udDGfQI-HxwufFVSKV0YQg";
-
-//  carga de peliculas populares 
 
 const options = {
     method: 'GET',
@@ -11,7 +19,7 @@ const options = {
     }
 };
 
-fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+fetch(`https://api.themoviedb.org/3/search/movie?query=${query}`, options)
     .then(response => response.json())
     .then(data => {
         const movies = data.results;
@@ -25,17 +33,6 @@ fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', option
             </div>
         `)
         .join('');
-        document.getElementById('peliculas-container').innerHTML = htmlResponse;
+        document.getElementById('busqueda-container').innerHTML = htmlResponse;
     })
     .catch(err => console.error(err));
-
-
-    document.getElementById('searchForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // evita que el formulario se envíe de forma tradicional
-
-    const movie_string= document.getElementById("buscadorById").value;
-    const movie_search= movie_string.replace(/\s+/g, '+'); //se remplazan los espacios del string por un +
-
-    // al buscar se redirecciona a movie_search.html con el parametro de busqueda en la URL "query"
-    window.location.href = `movie_search/movie_search.html?query=${movie_search}`;
-});
