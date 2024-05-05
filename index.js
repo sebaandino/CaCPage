3a00f0857195ae6df4629cf00e08221f86a00e3b
Andino Elias Sebastian Agustin <elias_andino@hotmail.com>
2024-05-04 21:00:05 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -17,21 +17,26 @@ fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', option
         const movies = data.results;
         const htmlResponse = movies.map(movie =>
             `
-            <div class="card" id="${movie.id} onclick=cargarPelicula(this)">
-                
-                    <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="movie poster">
-                    <span class="movie-title">${movie.title}</span>
-                
+            <div class="card" id="${movie.id}">
+                <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="movie poster">
+                <span class="movie-title">${movie.title}</span>
             </div>
         `)
         .join('');
         document.getElementById('peliculas-container').innerHTML = htmlResponse;
+
+        // Agregar escucha de eventos a los elementos de película después de que se renderizan
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            card.addEventListener('click', function() {
+                cargarPelicula(this);
+            });
+        });
     })
     .catch(err => console.error(err));
 
-
-    document.getElementById('searchForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // evita que el formulario se envíe de forma tradicional
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // evita que el formulario se envíe de forma tradicional
 
     const movie_string= document.getElementById("buscadorById").value;
     const movie_search= movie_string.replace(/\s+/g, '+'); //se remplazan los espacios del string por un +
