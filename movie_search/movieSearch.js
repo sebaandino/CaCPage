137f9e443c47cb93f9f9414721fa22651400a0ce
Andino Elias Sebastian Agustin <elias_andino@hotmail.com>
2024-05-05 13:22:30 -0300

    Actualizar movieSearch.js

--- movie_search/movieSearch.js
+++ movie_search/movieSearch.js
@@ -25,7 +25,7 @@ fetch(`https://api.themoviedb.org/3/search/movie?query=${query}`, options)
         const movies = data.results;
         const htmlResponse = movies.map(movie =>
             `
-            <div class="card">
+            <div class="card" id="${movie.id}">
                 <a href="#" class="movie">
                     <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="movie poster">
                     <span class="movie-title">${movie.title}</span>
@@ -36,3 +36,23 @@ fetch(`https://api.themoviedb.org/3/search/movie?query=${query}`, options)
         document.getElementById('busqueda-container').innerHTML = htmlResponse;
     })
     .catch(err => console.error(err));
+
+        // Agregar escucha de eventos a los elementos de película después de que se renderizan
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            card.addEventListener('click', function() {
+                cargarPelicula(this);
+            });
+        });
+    })
+    .catch(err => console.error(err));
+
+// Función para cargar la página HTML con la información de la película
+function cargarPelicula(elementoPelicula) {
+    // Obtener el ID de la película desde el atributo id del elemento de película
+    const movieId = elementoPelicula.id;
+    // Construir la URL de la página HTML que muestra la información de la película
+    const paginaURL = `movie-desc/movie-desc.html?query=${movieId}`;
+    // Redireccionar a la página HTML de la película seleccionada
+    window.location.href = paginaURL;
+}
