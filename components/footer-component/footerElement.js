1891dac31b320835a312bd022d1f2a9d0a05d1d1
sebaandino <elias_andino@hotmail.com>
2024-05-16 19:09:30 -0300

    se agrego el footer element

---
+++ components/footer-component/footerElement.js
@@ -0,0 +1,45 @@
+class footerElement extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = /*html*/ `
+
+            <style>
+
+footer {
+    background-image: linear-gradient(90deg, #9995 ,#2b90dd, #9995);
+    box-shadow: 0 0 3px #272727;
+    height: clamp(20px,10dvh,1000px);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 1.3rem;
+    width: 100%;
+}
+
+footer a{
+    color: white;
+}
+
+footer span{
+    text-wrap: pretty;
+}
+
+    @media screen and (width < 820px) {
+
+        footer{
+            position: relative;
+        }
+
+    } 
+        
+    </style>
+
+<footer>
+        <span>Derechos Reservados @ grupo 13</span>
+        <a href="credits.html">Creditos API</a>
+    </footer>
+        `;
+    }
+}
+
+customElements.define("footer-element", footerElement);
